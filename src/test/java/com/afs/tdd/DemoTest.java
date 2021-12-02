5c3e5abeea794e0cf72e93d284cff0fa7c24202f
Marcus Ho <marcus.ho@oocl.com>
2021-12-02 16:32:28 +0800

    [TTD practice 2][Marcus]feat:    testcase rotate left Case.

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DemoTest {
+
+    // Rotate to right
     @Test
     void should_direct_change_to_E_when_rotate_Mars_Rover_given_direction_N_and_command_R() {
         //given
@@ -53,6 +55,63 @@ class DemoTest {
         //given
         MarsRover marsRover = new MarsRover(0, 0, "W");
         String command = "R";
+
+        //when
+        marsRover.rotateMarsRover(command);
+        String result = marsRover.getStatus();
+
+        //return
+        assertEquals("0 0 N", result);
+    }
+
+    // Rotate to left
+    @Test
+    void should_direct_change_to_W_when_rotate_Mars_Rover_given_direction_N_and_command_L() {
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+        String command = "L";
+
+        //when
+        marsRover.rotateMarsRover(command);
+        String result = marsRover.getStatus();
+
+        //return
+        assertEquals("0 0 W", result);
+    }
+
+    @Test
+    void should_direct_change_to_S_when_rotate_Mars_Rover_given_direction_W_and_command_L() {
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, "W");
+        String command = "L";
+
+        //when
+        marsRover.rotateMarsRover(command);
+        String result = marsRover.getStatus();
+
+        //return
+        assertEquals("0 0 S", result);
+    }
+
+    @Test
+    void should_direct_change_to_E_when_rotate_Mars_Rover_given_direction_S_and_command_L() {
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, "S");
+        String command = "L";
+
+        //when
+        marsRover.rotateMarsRover(command);
+        String result = marsRover.getStatus();
+
+        //return
+        assertEquals("0 0 E", result);
+    }
+
+    @Test
+    void should_direct_change_to_N_when_rotate_Mars_Rover_given_direction_E_and_command_L() {
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, "E");
+        String command = "L";
 
         //when
         marsRover.rotateMarsRover(command);
