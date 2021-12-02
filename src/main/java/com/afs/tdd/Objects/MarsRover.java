8446c2e9c13632677d6395a42456605332ede1fb
Marcus Ho <marcus.ho@oocl.com>
2021-12-02 16:34:05 +0800

    [TTD practice 2][Marcus]feat:  rotate left Case.

--- src/main/java/com/afs/tdd/Objects/MarsRover.java
+++ src/main/java/com/afs/tdd/Objects/MarsRover.java
@@ -30,6 +30,9 @@ public class MarsRover {
         if (command.equals("R")) {
             direction = directionList.get((currentDirectionIndex + 1) > (directionList.size() - 1) ? 0 : directionList.indexOf(direction) + 1);
         }
+        if (command.equals("L")) {
+            direction = directionList.get((currentDirectionIndex - 1) < 0 ? directionList.size() - 1 : directionList.indexOf(direction) - 1);
+        }
     }
 }
 
