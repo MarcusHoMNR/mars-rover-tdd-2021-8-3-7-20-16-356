131461eb45c2b017280ed08f42d79c0fb5b57fdd
Marcus Ho <marcus.ho@oocl.com>
2021-12-02 16:30:34 +0800

    [TTD practice 2][Marcus]feat:    rotate right Case.

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -19,4 +19,46 @@ class DemoTest {
         //return
         assertEquals("0 0 E", result);
     }
+
+    @Test
+    void should_direct_change_to_S_when_rotate_Mars_Rover_given_direction_E_and_command_R() {
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, "E");
+        String command = "R";
+
+        //when
+        marsRover.rotateMarsRover(command);
+        String result = marsRover.getStatus();
+
+        //return
+        assertEquals("0 0 S", result);
+    }
+
+    @Test
+    void should_direct_change_to_W_when_rotate_Mars_Rover_given_direction_S_and_command_R() {
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, "S");
+        String command = "R";
+
+        //when
+        marsRover.rotateMarsRover(command);
+        String result = marsRover.getStatus();
+
+        //return
+        assertEquals("0 0 W", result);
+    }
+
+    @Test
+    void should_direct_change_to_N_when_rotate_Mars_Rover_given_direction_W_and_command_R() {
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, "W");
+        String command = "R";
+
+        //when
+        marsRover.rotateMarsRover(command);
+        String result = marsRover.getStatus();
+
+        //return
+        assertEquals("0 0 N", result);
+    }
 }
